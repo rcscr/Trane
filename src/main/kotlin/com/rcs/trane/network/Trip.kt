b388b96932a285eba0a1af725f8abc086e691826
Raphael Correa <julio123dog@gmail.com>
2024-06-19 00:01:56 -0300

    remove redundant word

--- src/main/kotlin/com/rcs/trane/network/Trip.kt
+++ src/main/kotlin/com/rcs/trane/network/Trip.kt
@@ -2,4 +2,4 @@ package org.example.com.rcs.trane.network
 
 import java.time.LocalDate
 
-data class Trip(val tripIndex: Int, val route: String, val date: LocalDate)
+data class Trip(val index: Int, val route: String, val date: LocalDate)
